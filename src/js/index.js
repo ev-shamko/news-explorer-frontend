8c7ad1acf725adaa4417ba7a2b730a52daa97d05
ev-shamko <ev-shamko@yandex.ru>
2020-09-04 10:03:26 +0300

    свёрстан попап

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,23 @@ import "../css/style.css";
 
 const menuOpenButton = document.querySelector('.header__menu-button');
 const menuInHeader = document.querySelector(".header__menu-container");
+const buttonCloseForm = document.querySelector(".popup__close");
+const popupBlock = document.querySelector(".popup");
+const buttonAuth = document.querySelector(".menu__auth-button");
 
-function openMenu() {
+function manageMenuVisibility() {
     menuInHeader.classList.toggle('header__menu-container_displayed');
     menuOpenButton.classList.toggle('header__menu-button_cross-white');
 }
 
-menuOpenButton.addEventListener('click', openMenu);
+function openForm() {
+    popupBlock.classList.add('popup_is-opened');
+}
+
+function closeForm() {
+    popupBlock.classList.remove('popup_is-opened');
+}
+
+menuOpenButton.addEventListener('click', manageMenuVisibility);
+buttonCloseForm.addEventListener('click', closeForm);
+buttonAuth.addEventListener('click', () => { openForm(); manageMenuVisibility(); });
