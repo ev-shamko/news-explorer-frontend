17e5e59d1b18f176673b156bd78757d6ae5d8eb5
ev-shamko <ev-shamko@yandex.ru>
2020-09-04 07:08:05 +0300

    сделан адаптив под 320

--- src/js/articles/index.js
+++ src/js/articles/index.js
@@ -1,2 +1,12 @@
 import "../../css/articles.css";
 
+const menuOpenButton = document.querySelector('.header__menu-button');
+const menuInHeader = document.querySelector(".header__menu-container");
+
+function openMenu() {
+    menuInHeader.classList.toggle('header__menu-container_displayed');
+    menuInHeader.classList.toggle('header__menu-container_grey-back');
+    menuOpenButton.classList.toggle('header__menu-button_cross-black');
+}
+
+menuOpenButton.addEventListener('click', openMenu);
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 import "../css/style.css";
+
+const menuOpenButton = document.querySelector('.header__menu-button');
+const menuInHeader = document.querySelector(".header__menu-container");
+
+function openMenu() {
+    menuInHeader.classList.toggle('header__menu-container_displayed');
+    menuOpenButton.classList.toggle('header__menu-button_cross-white');
+}
+
+menuOpenButton.addEventListener('click', openMenu);
